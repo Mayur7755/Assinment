14ff6acd98d63e1dadf94000d4d1afbf183594e1
Mayur7755 <42093688+Mayur7755@users.noreply.github.com>
2021-12-16 11:56:16 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,4 +3,4 @@
 echo "Hello World"
 # after save the code use chmod 755 file_name.
 #insead of 755, you can use +x means to give permission for execution.
-#or you can directly use bash file_name
+#or you can directly use bash file
